c741352e7788074b1e6f2dfd4d4ab2931d586da0
Alok Kumar <alokkumarkaranraj@gmail.com>
2025-04-11 12:06:24 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: process.env.REACT_APP_API_BASE_URL + '/api/jobs',
-  });  
+  baseURL: `${process.env.REACT_APP_API_BASE_URL}api/jobs`,
+  // ✅ Removed the extra slash before 'api/jobs'
+});
 
 export default API;
